e856a5ffbd6935cdf7d6176346f8a5b54bc0b7f0
Al-AizHashim <65592342+Al-AizHashim@users.noreply.github.com>
2021-02-06 12:15:37 +0300

    Bottom navigation is configured

--- app/src/main/java/alaiz/hashim/foodrecipes/MainActivity.kt
+++ app/src/main/java/alaiz/hashim/foodrecipes/MainActivity.kt
@@ -2,10 +2,35 @@ package alaiz.hashim.foodrecipes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteFragment
+            )
+        )
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+
+    }
+
 }
