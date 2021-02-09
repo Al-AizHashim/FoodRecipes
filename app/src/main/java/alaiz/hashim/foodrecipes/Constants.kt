dbe3b3b4b1e2707fb58ef568fd3c6511ef55f4e6
Al-AizHashim <65592342+Al-AizHashim@users.noreply.github.com>
2021-02-09 23:58:28 +0300

    Class of constants is added

---
+++ app/src/main/java/alaiz/hashim/foodrecipes/Constants.kt
@@ -0,0 +1,12 @@
+package alaiz.hashim.foodrecipes
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "[I REMOVED IT]"
+
+    }
+
+}
