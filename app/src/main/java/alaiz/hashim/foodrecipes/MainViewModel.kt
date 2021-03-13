89eacbae1e619812ce0f62c3554cedcf328fd17b
Al-AizHashim <65592342+Al-AizHashim@users.noreply.github.com>
2021-03-13 22:53:17 +0300

    Check internet connection function is added

--- app/src/main/java/alaiz/hashim/foodrecipes/MainViewModel.kt
+++ app/src/main/java/alaiz/hashim/foodrecipes/MainViewModel.kt
@@ -2,6 +2,9 @@ package alaiz.hashim.foodrecipes
 
 import alaiz.hashim.foodrecipes.data.Repository
 import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.AndroidViewModel
 
@@ -9,5 +12,19 @@ class MainViewModel @ViewModelInject constructor(
         private val repository: Repository,
         application: Application
 ) : AndroidViewModel(application) {
+
+    private fun hasInternetConnection(): Boolean {
+        val connectivityManager = getApplication<Application>().getSystemService(
+                Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+        return when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    }
 
 }
