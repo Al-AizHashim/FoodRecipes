24f5a5308c292e96421cf4ca2cf54379e7fdc7b6
Al-AizHashim <65592342+Al-AizHashim@users.noreply.github.com>
2021-03-17 23:46:58 +0300

    RecipesAdapter is created

---
+++ app/src/main/java/alaiz/hashim/foodrecipes/adapter/RecipesAdapter.kt
@@ -0,0 +1,11 @@
+package alaiz.hashim.foodrecipes.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RecipesAdapter: RecyclerView.Adapter<RecipesAdapter.MyViewHolder> {
+
+
+
+}
